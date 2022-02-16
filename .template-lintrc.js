33e48835f1768cbff1a7fa3ab0172e1e1460f9e0
Bert De Block <bert.deblock@bagaar.be>
2022-02-16 12:05:37 +0100

    chore: enable `modifier-name-case` rule again

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -2,9 +2,4 @@
 
 module.exports = {
   extends: ['a11y', 'recommended', 'stylistic'],
-  rules: {
-    // TEMP disable `modifier-name-case` until the following PR is released:
-    // https://github.com/ember-template-lint/ember-template-lint/pull/2348
-    'modifier-name-case': false,
-  },
 };
