db2c4fd314d77fd4d8a8c29f30f4671214436e9d
Bert De Block <bert.deblock@bagaar.be>
2022-08-11 15:00:41 +0200

    feat: refactor checkbox component

--- tailwind/components/checkbox.js
+++ tailwind/components/checkbox.js
@@ -1,82 +1,42 @@
 'use strict';
 
-const { reduceStyles } = require('../utils/styling');
-
-module.exports = ({ colors }) => ({
+module.exports = () => ({
   '.velvet-checkbox': {
-    '@apply flex items-center select-none w-fit': {},
+    '@apply flex items-center w-fit': {},
 
     '&-sm': {
-      '@apply text-sm': {},
+      '@apply text-xs': {},
 
       '.velvet-checkbox-input': {
-        '@apply h-3 mr-1 rounded-sm w-3': {},
+        '@apply h-4 mr-2 w-4': {},
       },
     },
 
     '&-md': {
-      '@apply text-base': {},
+      '@apply text-sm': {},
 
       '.velvet-checkbox-input': {
-        '@apply h-4 mr-2 rounded w-4': {},
+        '@apply h-5 mr-3 w-5': {},
       },
     },
 
     '&-lg': {
-      '@apply text-lg': {},
+      '@apply text-base': {},
 
       '.velvet-checkbox-input': {
-        '@apply h-5 mr-3 rounded-md w-5': {},
+        '@apply h-6 mr-4 w-6': {},
       },
     },
 
     '&-disabled': {
-      '@apply cursor-not-allowed opacity-40': {},
+      '@apply cursor-not-allowed': {},
     },
 
-    ...reduceStyles(colors, (color) => ({
-      [`&-${color}`]: {
-        '.velvet-checkbox-input': {
-          '&:focus': {
-            [`@apply border-${color}-400 ring-${color}-400/40`]: {},
-          },
-
-          '&:indeterminate': {
-            [`@apply bg-${color}-400`]: {},
-
-            '&:hover': {
-              [`@apply bg-${color}-400`]: {},
-            },
-
-            '&:focus': {
-              [`@apply bg-${color}-400`]: {},
-            },
-          },
-
-          '&:checked': {
-            [`@apply bg-${color}-400`]: {},
-
-            '&:hover': {
-              [`@apply bg-${color}-400`]: {},
-            },
-
-            '&:focus': {
-              [`@apply bg-${color}-400`]: {},
-            },
-          },
-        },
-      },
-    })),
-
     '&-input': {
-      '@apply bg-transparent border-gray-400/40 transition': {},
-
-      '&:not(:checked):not(:disabled):not(:focus):hover': {
-        '@apply border-gray-400/80': {},
-      },
+      '@apply border-2 rounded transition': {},
 
       '&:focus': {
-        '@apply ring ring-offset-0': {},
+        '@apply outline outline-2 outline-offset-1 ring-0 ring-offset-0': {},
       },
 
       '&:disabled': {
