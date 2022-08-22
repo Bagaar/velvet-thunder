7b2d16378c843985dbedd66a82bbf2958222a5a4
Bert De Block <bert.deblock@bagaar.be>
2022-08-22 11:17:08 +0200

    feat: add `tag` component

--- tailwind/plugin.js
+++ tailwind/plugin.js
@@ -13,6 +13,7 @@ const COMPONENTS = {
   radio: require('./components/radio'),
   select: require('./components/select'),
   spinner: require('./components/spinner'),
+  tag: require('./components/tag'),
   textarea: require('./components/textarea'),
 };
 
