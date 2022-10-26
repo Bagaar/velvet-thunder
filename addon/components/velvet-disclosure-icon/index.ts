c58e51c6d26b429af065b8b0e9c3976e07098d5a
Bert De Block <bert.deblock@bagaar.be>
2022-10-26 16:34:08 +0200

    chore: type-check `velvet-disclosure-icon` component

---
+++ addon/components/velvet-disclosure-icon/index.ts
@@ -0,0 +1,7 @@
+import templateOnlyComponent from '@ember/component/template-only';
+
+interface VelvetDisclosureIconComponentSignature {
+  Element: SVGElement;
+}
+
+export default templateOnlyComponent<VelvetDisclosureIconComponentSignature>();
