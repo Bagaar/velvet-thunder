9ddb0a9e2aaf9f1c9c134ddb69b59ae7ad92444c
Bert De Block <bert.deblock@bagaar.be>
2023-05-17 11:15:43 +0200

    chore: temp bring back `ember-cli-typescript`

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,7 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function (defaults) {
   const app = new EmberAddon(defaults, {
-    'ember-cli-babel': {
-      enableTypeScriptTransform: true,
-    },
+    // Add options here
   });
 
   /*
