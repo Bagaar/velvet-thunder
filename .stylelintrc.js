f117892dc51ceaec3bd8e20632f70c7fc007cabd
Bert De Block <bert.deblock@bagaar.be>
2023-09-04 20:53:07 +0200

    Resolve Prettier errors

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,10 +1,7 @@
 "use strict";
 
 module.exports = {
-  extends: [
-    "stylelint-config-standard",
-    "stylelint-config-tailwindcss",
-  ],
+  extends: ["stylelint-config-standard", "stylelint-config-tailwindcss"],
   rules: {
     "selector-class-pattern": null,
   },
--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,8 +1,5 @@
 "use strict";
 
 module.exports = {
-  extends: [
-    "a11y",
-    "recommended",
-  ],
+  extends: ["a11y", "recommended"],
 };
