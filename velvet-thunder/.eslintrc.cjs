0224d223b512957648d818eb8a656b07c5bbe4f8
gvdp <vdputteglenn@gmail.com>
2026-06-12 14:55:53 +0200

    chore: use new parser for eslinting gts files

--- velvet-thunder/.eslintrc.cjs
+++ velvet-thunder/.eslintrc.cjs
@@ -15,14 +15,23 @@ module.exports = {
   overrides: [
     // ts files
     {
-      files: ['**/*.ts', '**/*.gts'],
+      files: ['**/*.ts'],
       extends: [
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
       ],
-      rules: {
-        // Add any custom rules here
-      },
+      rules: {},
+    },
+    // gts files
+    {
+      files: ['**/*.gts'],
+      parser: 'ember-eslint-parser',
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:ember/recommended-gts',
+      ],
+      rules: {},
     },
     // node files
     {
